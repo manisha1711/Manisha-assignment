7c3c0519ce332031a3931a032de5a67fdd0be2cb
manisha1711 <manishachoudhary32@gmail.com>
2024-07-18 14:57:47 +0530

    Basic React App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import SampleData from './assets/data';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      hello
+    </>
+  )
+}
+
+export default App
